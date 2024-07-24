9fa3203c90c4c2a3bbfe1d4e493f7be5077770b1
Sagi Haklay <sahaklay@gmail.com>
2024-07-24 22:32:37 +0300

    added packages

--- functions/index.js
+++ functions/index.js
@@ -7,25 +7,32 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const {onCall, HttpsError} = require("firebase-functions/v2/https");
+const {logger} = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
 const {initializeApp} = require("firebase-admin/app");
-const {getFirestore, GeoPoint} = require("firebase-admin/firestore");
+const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
 
-exports.addvet = onRequest(async (req, res) => {
-    const vetName = req.query.name;
-    const writeResult = await getFirestore().collection("vets").add({
-        name: vetName,
-        image: 'https://via.placeholder.com/150',
-        position: new GeoPoint(32.072929, 34.776306)
-    });
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
+exports.addVaccination = onCall(async (request) => {
+    // Checking that the user is authenticated.
+    if (!request.auth) {
+        // Throwing an HttpsError so that the client gets the error details.
+        throw new HttpsError("failed-precondition", "The function must be " +
+                "called while authenticated.");
+    }
+    const vaccine = request.data;
+    const writeResult = await getFirestore()
+        .collection("vaccinations")
+        .add(vaccine);
+    logger.info("new vaccination added.");
+    return {
+        id: writeResult.id
+    };
 });
 // exports.helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
